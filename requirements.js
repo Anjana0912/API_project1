86c235c1cf5be6230ee11e7f14367d6ad365a885
Anjana0912 <anjana09venkatesh@gmail.com>
2021-08-06 17:53:02 +0530

    First API project

---
+++ requirements.js
@@ -0,0 +1,31 @@
+// requirement
+// book management company
+
+
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//We have to design and code an API over this .
+
+//1. BOOKS
+//We need an API :-
+//To get all the books
+//To get specific book
+//To get a list of books based on category
+//To get a list of books based on languages //task DONE
+
+//2. AUTHORS
+//We need an API :-
+//To get all the authors
+//To get a specific author//task DONE
+//To get a list of authors based on books
+
+//3. PUBLICATIONS
+//We need an API :-
+//To get all the publications
+//To get a specific publication //task DONE
+//To get a list of publications based on a book//task DONE
